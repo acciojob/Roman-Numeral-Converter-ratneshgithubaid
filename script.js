f7ab5d5695ef104f12b80f01ab3383bf1bd701c6
ratneshgithubaid <140415763+ratneshgithubaid@users.noreply.github.com>
2025-06-20 09:12:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const numberInput = document.getElementById('number-input');
-    const convertButton = document.getElementById('convert-button');
-    const resultOutput = document.getElementById('result-output');
+function convertToRoman(num) {
+  const romanNumerals = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-    convertButton.addEventListener('click', function() {
-        const number = parseInt(numberInput.value);
-        
-        if (isNaN(number) || number < 1 || number > 100000) {
-            resultOutput.textContent = 'Please enter a number between 1 and 100,000';
-            return;
-        }
-        
-        const romanNumeral = convertToRoman(number);
-        resultOutput.textContent = romanNumeral;
-    });
+  let result = '';
 
-    function convertToRoman(num) {
-        if (num === 0) return '';
-        
-        const romanSymbols = [
-            ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
-            ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
-            ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]
-        ];
-        
-        let result = '';
-        
-        for (let [symbol, value] of romanSymbols) {
-            while (num >= value) {
-                result += symbol;
-                num -= value;
-            }
-        }
-        
-        return result;
+  for (const [roman, value] of romanNumerals) {
+    while (num >= value) {
+      result += roman;
+      num -= value;
     }
-});
+  }
+
+  return result;
+}
+
+// Example test:
+console.log(convertToRoman(14)); // Output: XIV
+// console.log(convertToRoman(36)); // Output: XXXVI
+
+// do not edit below this line
+module.exports = convertToRoman;
